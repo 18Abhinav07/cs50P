bc39d50a5309ee930f5e58703d9fd6eb5cc56ddf
Abhinav Pangaria <abhinavpangaria2003@gmail.com>
2024-03-14 21:45:37 +0530

    Update emojize.py

--- week4/emojize/emojize.py
+++ week4/emojize/emojize.py
@@ -1,5 +1,13 @@
-import emoji
+from emoji import emojize
 
-input = input("Input: ")
 
-print("Output:", emoji.emojize(input))
+def main():
+    # get the user input
+    user_input = input("Input: ").strip()
+
+    # print the user input with the given emoji
+    print("Output: " + emojize(user_input, language="alias"))
+
+
+if __name__ == "__main__":
+    main()
